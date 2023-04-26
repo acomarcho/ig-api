38b4311fa35d91c581162900bd39ab5913bf3fd6
acomarcho <acoxstpd@gmail.com>
2023-04-26 12:00:04 +0700

    Feat: Service now returns general user data

--- services/ig.js
+++ services/ig.js
@@ -9,7 +9,22 @@ const getUserData = async (username) => {
       )
       .set("x-ig-app-id", process.env.X_IG_APP_ID);
 
-    return response.body.data;
+    const { user } = response.body.data;
+    // General data
+    const followers_count = user.edge_followed_by.count;
+    const following_count = user.edge_follow.count;
+    const full_name = user.full_name;
+    const profile_pic_url = user.profile_pic_url;
+    const profile_pic_url_hd = user.profile_pic_url_hd;
+
+    return {
+      username,
+      followers_count,
+      following_count,
+      full_name,
+      profile_pic_url,
+      profile_pic_url_hd,
+    };
   } catch (error) {
     console.log("Error", error);
     throw error;
